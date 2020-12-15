f9b01c2ca5b97ac5e5dc0d19e5435f84d60d1304
Giampiero Fornaglioni <bonetti.mrc@gmail.com>
2020-12-15 16:15:27 +0100

    changed repeat command

--- commands/repeat.js
+++ commands/repeat.js
@@ -2,6 +2,10 @@ module.exports = {
 	name: 'repeat',
 	description: 'It repeats the message from the person who invoked the command.',
 	execute(message, args) {
-		message.channel.send(args.toString(), { tts: true })
+		let string = ""
+		for (arg of args) {
+			string = string.concat(arg)
+		}
+		message.channel.send(string, { tts: true })
 	},
 };
