11883c76548e723dde81007b32a8d46fbe1cf182
Giampiero Fornaglioni <bonetti.mrc@gmail.com>
2020-12-15 16:32:30 +0100

    fixed summon command

--- commands/summon.js
+++ commands/summon.js
@@ -1,7 +1,7 @@
 module.exports = {
 	name: 'summon',
 	description: 'calls a person with tts. ex: ioana summon <person\'s name>',
-	execute(message, args) {
+	execute(message, args, otherArgs) {
 		const summon = require(otherArgs.dirname + '\\commands\\repeat')
 		args.push(", ti evoco")
 		summon.execute(message, args)
