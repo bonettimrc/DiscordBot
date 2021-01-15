0b3ddf9cb7795c0f86f3d64c0dc2170cd19cc0af
bonetti.mrc <bonetti.mrc@gmail.com>
2021-01-15 14:43:36 +0100

    dadjoke command

---
+++ commands/dadjoke.js
@@ -0,0 +1,33 @@
+const https = require('https');
+module.exports = {
+    name: 'dadjoke',
+    group: 'inutilities',
+    active: true,
+    description: 'sends a dadjoke',
+    execute(message, args) {
+        const options = {
+            host: 'icanhazdadjoke.com',
+            headers: {
+                'Accept': "application/json"
+            }
+        };
+        https.request(options, (response) => {
+            var str = '';
+
+            //another chunk of data has been received, so append it to `str`
+            response.on('data', function (chunk) {
+                str += chunk;
+            });
+
+            //the whole response has been received, so we just print it out here
+            response.on('end', function () {
+                dadjoke = JSON.parse(str).joke
+                if (args[0] === "tts") { message.reply(dadjoke, { tts: true }) }
+                else {
+                    message.reply(dadjoke)
+                }
+            });
+        }).end();
+
+    }
+}
